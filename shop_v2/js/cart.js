8d9aefe5201afd2755540079dbd158c112396595
Corjik <Corjik@mbp-alexey.rsmtopaudit.com>
2017-06-21 12:51:27 +0300

    3.2 lesson upgrade

--- shop_v2/js/cart.js
+++ shop_v2/js/cart.js
@@ -1,25 +1,15 @@
 'use strict';
-const more = document.querySelector('.show-more');
-more.addEventListener('click', showMoreFun);
-cartBtn();
+const itemsList = document.querySelector('.items-list')
+itemsList.addEventListener('click', makeItemNew)
 
-function cartBtn() {
-  let links = document.querySelectorAll('main a');
-  for (let link of links){
-    link.addEventListener('click', makeItem);
-  };
-};
-
-function makeItem (event) {
+function makeItemNew(event) {
   event.preventDefault();
   let elem = event.target;
-  let item = {};
-  item.title = elem.dataset.title;
-  item.price = elem.dataset.price;
-  addToCart(item);
+  let elemName = event.target.nodeName;
+  if ( elemName === 'A') {
+    let item = {};
+    item.title = elem.dataset.title;
+    item.price = elem.dataset.price;
+    addToCart(item);
+  };
 };
-
-function showMoreFun (event) {
-  event.preventDefault();
-  cartBtn();
-}
